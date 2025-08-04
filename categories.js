26eb94c415bfdf6156a99cb991108c5a4bcfa151
leehaoze <leehaoze2022@gmail.com>
2025-08-04 19:39:35 +0800

    refactor(categories): 移除多个网站分类仅保留搜索引擎
    其他分类已被注释掉以便后续可能恢复使用

--- categories.js
+++ categories.js
@@ -22,71 +22,8 @@ function loadCategories() {
           { name: "Google", url: "https://www.google.com", icon: "" },
           { name: "夸克搜索", url: "https://quark.sm.cn", icon: "" }
         ]
-      },
-      {
-        name: "综合电商",
-        websites: [
-          { name: "淘宝", url: "https://www.taobao.com", icon: "" },
-          { name: "京东", url: "https://www.jd.com", icon: "" },
-          { name: "天猫", url: "https://www.tmall.com", icon: "" },
-          { name: "苏宁易购", url: "https://www.suning.com", icon: "" }
-        ]
-      },
-      {
-        name: "社交媒体",
-        websites: [
-          { name: "微博", url: "https://weibo.com", icon: "" },
-          { name: "QQ空间", url: "https://qzone.qq.com", icon: "" },
-          { name: "知乎", url: "https://www.zhihu.com", icon: "" },
-          { name: "豆瓣", url: "https://www.douban.com", icon: "" }
-        ]
-      },
-      {
-        name: "视频平台",
-        websites: [
-          { name: "抖音", url: "https://www.douyin.com", icon: "" },
-          { name: "哔哩哔哩", url: "https://www.bilibili.com", icon: "" },
-          { name: "优酷", url: "https://www.youku.com", icon: "" },
-          { name: "腾讯视频", url: "https://v.qq.com", icon: "" },
-          { name: "爱奇艺", url: "https://www.iqiyi.com", icon: "" }
-        ]
-      },
-      {
-        name: "新闻资讯",
-        websites: [
-          { name: "新浪新闻", url: "https://news.sina.com.cn", icon: "" },
-          { name: "腾讯新闻", url: "https://news.qq.com", icon: "" },
-          { name: "人民网", url: "http://www.people.com.cn", icon: "" },
-          { name: "凤凰新闻", url: "https://news.ifeng.com", icon: "" },
-          { name: "新华网", url: "http://www.xinhuanet.com", icon: "" }
-        ]
-      },
-      {
-        name: "学习与教育",
-        websites: [
-          { name: "网易云课堂", url: "https://study.163.com", icon: "" },
-          { name: "学堂在线", url: "https://www.xuetangx.com", icon: "" },
-          { name: "中国大学MOOC", url: "https://www.icourse163.org", icon: "" },
-          { name: "学习通", url: "https://chaoxing.com", icon: "" },
-        ]
-      },
-      {
-        name: "开发工具",
-        websites: [
-          { name: "Github", url: "https://github.com", icon: "" },
-          { name: "Vs Code", url: "https://visualstudio.microsoft.com/zh-hans/", icon: "" },
-          { name: "IntelliJ IDEA", url: "https://www.jetbrains.com.cn/idea/", icon: "" },
-          { name: "Docker", url: "https://www.docker.com/", icon: "" },
-        ]
-      },
-      {
-        name: "AI网站",
-        websites: [
-          { name: "豆包", url: "https://doubao.com", icon: "" },
-          { name: "秘塔AI", url: "https://metaso.cn", icon: "" },
-          { name: "Kimi Ai", url: "https://kimi.moonshot.cn", icon: "" },
-        ]
       }
+      // 其他分类...
     ];
     saveCategories();
   }
